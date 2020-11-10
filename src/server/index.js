aae68a78ecd8b7b9d76ba5304676b545fe690d58
yklee921003 <71996900+yklee921003@users.noreply.github.com>
2020-11-09 21:57:03 -0500

    change API
    add rover_name

--- src/server/index.js
+++ src/server/index.js
@@ -12,19 +12,19 @@ app.use(bodyParser.json())
 
 app.use('/', express.static(path.join(__dirname, '../public')))
 
-// your API calls
+//  API calls
 // Mission Manifest
 app.get("/roverInfo/:rover_name", async (req, res) => {
   const url = "https://api.nasa.gov/mars-photos/api/v1/"
-
   try {
     const dataResponse = await fetch(`${url}manifests/${req.params.rover_name}?api_key=${process.env.API_KEY}`)
           .then(res => res.json())
           res.send({dataResponse})
     let max_date = dataResponse.photo_manifest.max_date
-    let roverPhotos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${req.params.rover_name}/photos?earth_date=${max_date}&api_key=${process.env.API_KEY}`)
+    let rover_name = dataResponse.photo_manifest.name
+    let roverPhotos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover_name}/photos?earth_date=${max_date}&page=1&api_key=${process.env.API_KEY}`)
       .then(res => res.json())
-       res.send(roverPhotos)
+       // res.send(roverPhotos)
   } catch (err){
     console.log('error:', err);
   }
@@ -36,15 +36,43 @@ app.get('/apod', async (req, res) => {
     try {
         let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
-        res.send({ image })
+        res.send( { image } )
+
     } catch (err) {
         console.log('error:', err);
     }
 })
 
-// app.get("/rover/roverName", async(req, res) => {
-//   try {
+// app.get('/rover/:name', async(req, res) =>{
 //     let roverName = req.params.name.roLowerCase()
+//     let date
+//     let url
+//
+//     if (roverName ==='opportunity'){
+//       date = '2018-06-04'
+//         url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?earth_date=${date}&api_key=${process.env.API_KEY}`
+//     }
+//     if(roverName ==='spirit'){
+//       date = '2010-02-01'
+//       url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?earth_date=${date}&api_key=${process.env.API_KEY}`
+//     }else if(roverName === 'curiosity'){
+//       url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/latest_photos?api_key=${process.env.API_KEY}`
+//     }else {
+//       res.send ('address is invalid')
+//       return
+//     }
+//     try {
+//       const data = await fetch(url)
+//             .then (res => res.json())
+//             res.send({data})
+//     } catch (err){
+//       console.log('error:', err);
+//     }
+//
+// })
+// app.get("/rover/curiosity", async(req, res) => {
+//   try {
+//     // let roverName = req.params.name.roLowerCase()
 //     let url = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?sol=1000&api_key=${process.env.API_KEY}`)
 //           .then(res => res.json())
 //           res.send({url})
@@ -56,7 +84,7 @@ app.get('/apod', async (req, res) => {
 //
 // app.get('/rover/opportunity', async(req, res) => {
 //   try {
-//     let roverName = req.params.name.roLowerCase()
+//     // let roverName = req.params.name.roLowerCase()
 //     let url = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?sol=1000&api_key=${process.env.API_KEY}`)
 //           .then(res => res.json())
 //           res.send({url})
@@ -68,7 +96,7 @@ app.get('/apod', async (req, res) => {
 //
 // app.get('/rover/spirit', async(req, res) => {
 //   try{
-//     let roverName = req.params.name.roLowerCase()
+//     // let roverName = req.params.name.roLowerCase()
 //     let url = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?sol=1000&api_key=${process.env.API_KEY}`)
 //         .then(res => res.json())
 //         res.send({url})
